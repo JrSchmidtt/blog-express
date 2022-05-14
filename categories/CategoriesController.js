c8a407c17374df08c50112d41f5cef664193546e
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-14 03:21:06 -0300

    Categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Category = require('./Category')
-const slugify = require('slugify')
+const Category = require('./Category');
+const slugify = require('slugify');
 
 router.get('/categories', (req, res) => {
     res.send('ROTA DE CATEGORIAS');
@@ -14,7 +14,6 @@ router.get('/admin/categories/new', (req, res) => {
 router.post('/admin/categories/save', (req, res) => {
     var title = req.body.title;
     if(title != undefined){
-
         Category.create({
             title: title,
             slug: slugify(title)
@@ -25,6 +24,12 @@ router.post('/admin/categories/save', (req, res) => {
     }else{
         res.redirect('/admin/categories/new');
     }
+});
+
+router.get('/admin/categories', (req, res) => {
+    Category.findAll().then(categories => {
+        res.render('admin/categories/index', {categories : categories});
+    });
 });
 
 module.exports = router;
