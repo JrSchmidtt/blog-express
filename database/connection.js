686d38e87cc93687837d01031a41b0df2c6fcda0
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-21 17:46:41 -0300

    Deletar Categorias

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const connection = new Sequelize('blog-express','root','0808',{
+const connection = new Sequelize('blog-express','root','',{
     host:'localhost',
     dialect:'mysql'
 });
