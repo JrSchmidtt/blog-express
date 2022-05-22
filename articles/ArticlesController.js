3ee2775645b1c19d8ab90ad910a905dafc3d32ee
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-22 14:45:56 -0300

    Articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
-const Articles = require('./Article');
-const slugify = require('slugify');
 const Article = require('./Article');
+const slugify = require('slugify');
 
 router.get('/admin/articles', (req, res) => {
-    res.send('ROTA DE ARTIGOS');
+    Article.findAll({
+        include: [{model:Category}]
+    }).then(articles => {
+        res.render('admin/articles/index', {articles: articles})
+    });
 });
 
 router.get('/admin/articles/new', (req, res) => {
@@ -22,13 +25,32 @@ router.post('/articles/save', (req, res)=> {
 
     Article.create({
         title: title,
-        slug: slugify('title'),
+        slug: slugify(title),
         body: body,
         categoryId: category
     }).then(() =>{
         res.redirect('/admin/articles');
     })
 
+});
+
+router.post('/articles/delete', (req, res)=> {
+    var id = req.body.id;
+    if (id != undefined) {
+        if(!isNaN(id)) { 
+            Article.destroy({
+                where:{
+                    id:id
+                }
+            }).then(() => {
+                res.redirect('/admin/articles');
+            })
+        }else{ 
+            res.redirect('/admin/articles');
+        }
+    }else{ 
+        res.redirect('/admin/articles');
+    }
 });
 
 module.exports = router;
