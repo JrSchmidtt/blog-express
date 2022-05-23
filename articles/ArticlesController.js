8e1d5a5b71ff43fa0029dc82931303efed8cea13
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-22 21:37:45 -0300

    CRUD Articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -53,4 +53,39 @@ router.post('/articles/delete', (req, res)=> {
     }
 });
 
+router.get('/admin/articles/edit/:id', (req, res) =>{
+    var id = req.params.id;
+    Article.findOne({
+        where: {
+            id:id
+        },
+    }).then(article =>{
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render('admin/articles/edit', {categories:categories,article:article, });
+            })
+        }else{
+            res.redirect('/admin/articles');
+        }
+    }).catch(erro => {
+    })
+});
+
+router.post('/articles/update', (req, res,) =>{
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({title: title, slug:slugify(title), body:body, categoryId:category},{
+        where: {
+            id:id
+        }
+    }).then(() =>{
+        res.redirect('/admin/articles');
+    }).catch(err => {
+        res.redirect('/');
+    })
+});
+
 module.exports = router;
