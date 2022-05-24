50a9ac33983ef5554120edb9e709bb5874a6d79a
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-23 22:17:11 -0300

    User Authenticate

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/connection');
+const session = require('express-session');
 
 const categoriesController = require('./categories/CategoriesController');
 const articlesContoller = require('./articles/ArticlesController')
+const usersController = require('./users/UsersController')
+
 const Article = require('./articles/Article');
 const Category = require('./categories/Category');
+const User = require('./users/User')
 
 // View Engine
 app.set('view engine', 'ejs');
+
+// Sessions
+app.use(session({
+    secret: '!@&l#umsKIgqI$XLViBaF^8$FS#mlpNJ@@*SZ5pjRfArrfBDrQ',
+    cookie: {maxAge: 300000000}, // tempo maximo da sessão em 30 segundos
+    resave: true,
+    saveUninitialized: true
+}))
 
 // Static
 app.use(express.static('public'));
@@ -31,24 +43,11 @@ app.get('/', (req, res) =>{
     });
 })
 
-app.get('/:slug', (req, res) =>{
-    var slug = req.params.slug;
-    Article.findOne({
-        where: {
-            slug:slug
-        }
-    }).then(article =>{
-        if(article != undefined){
-            Category.findAll().then(categories => {
-                res.render('article', {article:article, categories:categories});
-            })
-        }else{
-            res.redirect('/');
-        }
-    }).catch(err =>{
-        res.redirect('/');
-    })
-})
+// Routers
+app.use('/',categoriesController);
+app.use('/',articlesContoller);
+app.use('/',usersController);
+
 
 app.get('/category/:slug', (req, res) =>{
     var slug = req.params.slug;
@@ -70,9 +69,24 @@ app.get('/category/:slug', (req, res) =>{
     })
 })
 
-// Routers
-app.use('/',categoriesController);
-app.use('/',articlesContoller);
+app.get('/:slug', (req, res) =>{
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug:slug
+        }
+    }).then(article =>{
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render('article', {article:article, categories:categories});
+            })
+        }else{
+            res.redirect('/');
+        }
+    }).catch(err =>{
+        res.redirect('/');
+    })
+})
 
 // database MYSQL
 connection
